52746cd6f98e27edbfdea9b6cb9379d1842fa331
romanmuni8 <romanmuni8@gmail.com>
2023-09-01 09:39:47 -0400

    Initial commit

---
+++ ios-bootcamp-homeworks/ios-bootcamp-homeworks/ios_bootcamp_homeworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_bootcamp_homeworksApp.swift
+//  ios-bootcamp-homeworks
+//
+//  Created by Roman on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_bootcamp_homeworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
