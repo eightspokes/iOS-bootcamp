517ac5459da7e7147bbd499abc1be0a9fc16599c
romanmuni8 <romanmuni8@gmail.com>
2023-10-29 20:10:41 -0400

    HW 9 done

---
+++ ios-bootcamp-homeworks/week9/PexelsImage/PexelsImage/PhotoCard.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCard.swift
+//  PexelsImage
+//
+//  Created by Roman on 10/29/23.
+//
+
+import SwiftUI
+
+struct PhotoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhotoCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCard()
+    }
+}
